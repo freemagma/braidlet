23c498fbc251ce5dfea2ba7c6e08987967624806
Ubuntu <ubuntu@ip-172-31-18-94.ec2.internal>
2021-04-15 20:44:00 +0000

    sage compat

--- flaskapp/queries.py
+++ flaskapp/queries.py
@@ -1,30 +1,33 @@
-from math_braid import Braid
+from sage.all_cmdline import *
 
 
 def word_problem(b1):
-    bb1, bb2 = create_braid_objects(b1, [])
-    result = bb1 == bb2
+    sb1, sb2 = create_braid_objects(b1, [])
+    result = sb1 == sb2
     phrase = "is" if result else "is not"
     return f"{b1} {phrase} the identity"
 
 
 def equal(b1, b2):
-    bb1, bb2 = create_braid_objects(b1, b2)
-    result = bb1 == bb2
+    sb1, sb2 = create_braid_objects(b1, b2)
+    result = sb1 == sb2
     phrase = "is" if result else "is not"
     return f"{b1} {phrase} equal to {b2}"
 
 
 def conjugacy_problem(b1, b2):
-    bb1, bb2 = create_braid_objects(b1, b2)
-    result = False
-    phrase = "are" if result else "are not"
-    return f"{b1} and {b2} {phrase} conjugates"
+    sb1, sb2 = create_braid_objects(b1, b2)
+    if sb1.is_conjugated(sb2):
+        conj = sb1.conjugating_braid(sb2)
+        return f"{b1} and {b2} are conjugates via {conj}"
+    else:
+        return f"{b1} and {b2} are not conjugates"
 
 
 def create_braid_objects(*braids):
-    n = max(max(map(abs, b), default=0) for b in braids) + 1
-    return tuple(Braid(b, n) for b in braids)
+    n = 1 + max(max(map(abs, b), default=1) for b in braids)
+    B = BraidGroup(n, [f's{x}' for x in range(1, n)])
+    return tuple(map(B, braids))
 
 
 DEFAULT_INIT_QUERY = "Word Problem"
