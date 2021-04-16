98236299ddee958d2663d4013775e0217631bf60
Charles Gunn <cjgunnmail@gmail.com>
2021-04-16 14:50:47 -0400

    some bigfixes

--- flaskapp/queries.py
+++ flaskapp/queries.py
@@ -24,16 +24,16 @@ def conjugacy_problem(b1, b2):
         return f"{b1} and {b2} are not conjugates"
 
 
-def lcm(b1,b2):
+def lcm(b1, b2):
     sb1, sb2 = create_braid_objects(b1, b2)
     lcm = sb1.lcm(sb2)
-    return f"lcm({sb1},{sb2}) = {lcm}"
+    return f"lcm({b1},{b2}) = {lcm}"
 
 
-def gcd(b1,b2):
+def gcd(b1, b2):
     sb1, sb2 = create_braid_objects(b1, b2)
     gcd = sb1.gcd(sb2)
-    return f"lcm({sb1},{sb2}) = {gcd}"
+    return f"gcd({b1},{b2}) = {gcd}"
 
 
 ######
@@ -41,59 +41,58 @@ def gcd(b1,b2):
 ######
 
 
-#NOTE: the sizee of the braid group is important here
+# NOTE: the size of the braid group is important here
 def alexander_poly(b1):
-    sb1, sb2 = create_braid_objects(b1, [])
+    sb1 = create_braid_objects(b1)
     poly = sb1.alexander_polynomial()
-    return f"The Alexander polynomial of the closure of {sb1} is {poly}"
+    return f"The Alexander polynomial of the closure of {b1} is {poly}"
 
 
 def jones_poly(b1):
-    sb1, sb2 = create_braid_objects(b1, [])
+    sb1 = create_braid_objects(b1)
     poly = sb1.jones_polynomial()
-    return f"The Jones polynomial of the closure of {sb1} is {poly}"
+    return f"The Jones polynomial of the closure of {b1} is {poly}"
 
 
 ######
 ######  Representations
 ######
 
-def Burau_unreduced(b1):
-     sb1, sb2 = create_braid_objects(b1, [])
-     matrix = sb1.burau_matrix()
-     return f"The unreduced Burau representation of {sb1} is \n{matrix}"
+
+def burau_unreduced(b1):
+    sb1, sb2 = create_braid_objects(b1, [])
+    matrix = sb1.burau_matrix()
+    return f"The unreduced Burau representation of {b1} is \n{matrix}"
 
 
-def Burau_reduced(b1):
-     sb1, sb2 = create_braid_objects(b1, [])
-     matrix = sb1.burau_matrix(var='t',True)
-     return f"The unreduced Burau representation of {sb1} is \n{matrix}"
+def burau_reduced(b1):
+    sb1, sb2 = create_braid_objects(b1, [])
+    matrix = sb1.burau_matrix(reduced=True)
+    return f"The reduced Burau representation of {b1} is \n{matrix}"
 
 
 def LKB(b1):
     sb1, sb2 = create_braid_objects(b1, [])
     matrix = sb1.LKB_matrix()
-    return f"The Lawrence-Krammer-Bigelow representation of {sb1} is \n{matrix}"
+    return f"The Lawrence-Krammer-Bigelow representation of {b1} is \n{matrix}"
 
 
-#note: this is a *right* action of Bn on Fn
-def Aut_Fn_representation(b1):
+# note: this is a *right* action of Bn on Fn
+def aut_Fn_representation(b1):
     sb1, sb2 = create_braid_objects(b1, [])
     num_strands = sb1.strands()
     F = FreeGroup(num_strands)
     gens = list(F.generators())
     images = [gen * sb1 for gen in gens]
-    return f"The action of {sb1} on F_{num_strands} is given by {gens} -> {images}"
+    return f"The action of {b1} on F_{num_strands} is given by {gens} -> {images}"
 
 
-
-##what is the "drain size"...?
+# what is the "drain size"...?
 
 # def TL(b1):
 #     sb1, sb2 = create_braid_objects(b1, [])
 #     matrix = sb1.TL_matrix()
 #     return f"The Temperley–Lieb–Jones representation of {sb1} is \n{matrix}"
-
 
 
 ######
@@ -102,71 +101,72 @@ def Aut_Fn_representation(b1):
 
 
 def NT_type(b1):
-    sb1, sb2 = create_braid_objects(b1, [])
+    sb1 = create_braid_objects(b1)
     nt_type = sb1.thurston_type()
-    return f"{sb1} is {nt_type}"
+    return f"{b1} is {nt_type}"
 
 
 def induced_permutation(b1):
-    sb1, sb2 = create_braid_objects(b1, [])
+    sb1 = create_braid_objects(b1)
     permutation = sb1.permutation()
-    one_to_n = [i for i in range(1,sb1.strands()+1)]
-    return f"The permutation induced by {sb1} is {one_to_n} -> {permutation}"
+    one_to_n = [i for i in range(1, sb1.strands() + 1)]
+    return f"The permutation induced by {b1} is {one_to_n} -> {permutation}"
 
 
 def gens_centralizer(b1):
-    sb1, sb2 = create_braid_objects(b1, [])
+    sb1 = create_braid_objects(b1)
     gens = sb1.centralizer()
-    to_return = "The elements\n"
-    for gen in gens:
-        to_return += str(gen) +",\n"
-    return to_return
+    return "The centralizer generators are:\n" + ",\n".join(map(str, gens))
+
 
 def num_components(b1):
-    sb1, sb2 = create_braid_objects(b1, [])
+    sb1 = create_braid_objects(b1)
     components = sb1.components_in_closure()
-    return f"{sb1} has {components} in it's link closure"
+    return f"{b1} has {components} components in it's link closure"
 
+
+######
+######  Helper Functions
 ######
 
 
 def create_braid_objects(*braids):
     n = 1 + max(max(map(abs, b), default=1) for b in braids)
-    B = BraidGroup(n, [f's{x}' for x in range(1, n)])
+    B = BraidGroup(n, [f"s{x}" for x in range(1, n)])
     return tuple(map(B, braids))
 
 
 DEFAULT_INIT_QUERY = "Word Problem"
 QUERIES = {
     "Aut(F_n) Representation": 1,
-    #"Temperley–Lieb–Jones Representation": 1,
+    # "Temperley–Lieb–Jones Representation": 1,
     "Lawrence-Krammer-Bigelow Representation": 1,
     "Components in Closure": 1,
     "Generators of the Centralizer": 1,
     "Induced Permutation": 1,
     "Nielsen-Thurston type": 1,
     "Burau (unreduced)": 1,
-    "Burau (reduced)": 1, 
+    "Burau (reduced)": 1,
     "Alexander Polynomial": 1,
-    "Jones Polynomial": 1, 
-    "LCM": 2, 
-    "GCD": 2, 
-    "Word Problem": 1, 
-    "Conjugacy Problem": 2, 
-    "Equal": 2
+    "Jones Polynomial": 1,
+    "LCM": 2,
+    "GCD": 2,
+    "Word Problem": 1,
+    "Conjugacy Problem": 2,
+    "Equal": 2,
 }
 
 
 QUERY_FUNCS = {
-    "Aut(F_n) Representation": Aut_Fn_representation,
-    #"Temperley–Lieb–Jones Representation": TL,
+    "Aut(F_n) Representation": aut_Fn_representation,
+    # "Temperley–Lieb–Jones Representation": TL,
     "Lawrence-Krammer-Bigelow Representation": LKB,
     "Components in Closure": num_components,
     "Generators of the Centralizer": gens_centralizer,
     "Induced permutation": induced_permutation,
     "Nielsen-Thurston type": NT_type,
-    "Burau (unreduced)": Burau_unreduced,
-    "Burau (reduced)": Burau_reduced,
+    "Burau (unreduced)": burau_unreduced,
+    "Burau (reduced)": burau_reduced,
     "Jones polynomial": jones_poly,
     "Alexander polynomial": alexander_poly,
     "GCD": gcd,
